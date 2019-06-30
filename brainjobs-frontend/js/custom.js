2654fa906504cd4883e8506ca72053acd3afe5c9
dlcgold <dlcgold96@gmail.com>
2019-07-01 00:02:46 +0200

    bug fix empty data

--- brainjobs-frontend/js/custom.js
+++ brainjobs-frontend/js/custom.js
@@ -8,44 +8,73 @@ $(document).ready(function() {
 
         var user_id = $("#user_id_search").val();
 
-        $.ajax({
+        if( !user_id ) {
+            $('#results').html('<div class="alert alert-warning alert-dismissible fade show" role="alert">'
+                +'<strong>Warning!</strong> Compila il campo user_id.'
+                +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+                    +'<span aria-hidden="true">&times;</span>'
+                +'</button>'
+            +'</div>');
+        }
+        else {
 
-            type: "GET",
-            url: API + 'jobs?user_id=' + user_id,
-            dataType: "json",
-            success: function(data) {
+            $.ajax({
 
-                var result = "";
-                $(data).each(function(index, item) {
-					result += "<i>Richiesta " + (index+1) + "</i> <br> ";
-                    result += "user_id: " + item.user_id + " <br> ";
-                    result += "title: " + item.title + " <br> ";
-					result += "language: " + item.language + " <br> ";
-					result += "framework: " + item.framework + " <br> ";
-					result += "dataset: " + item.dataset + " <br> ";
-					result += "dataset_datatype: " + item.dataset_datatype + " <br> ";
-					result += "model: " + item.model + " <br> ";
-					result += "status: " + item.status + " <br> ";
-					result += "created_at: " + item.created_at + " <br> ";
-					result += "job_id: " + item.job_id + " <br> ";
-					result += "<br>";
+                type: "GET",
+                url: API + 'jobs?user_id=' + user_id,
+                dataType: "json",
+                success: function(data) {
+    
+                    
 
-				});
+                    if(data.length == 0){
+                        $('#results').html('<div class="alert alert-warning alert-dismissible fade show" role="alert">'
+                                +'<strong>Warning!</strong> Nessuna richiesta per lo user selezionato.'
+                                +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+                                    +'<span aria-hidden="true">&times;</span>'
+                                +'</button>'
+                            +'</div>');
+                    }
+                    else{
 
-                $('#results').html('<div class="alert alert-info" role="alert">'
-                    +'<strong>Dati richiesti</strong> <br><br>'+result
-                +'</div>');
-            },
-            error: function() {
-                $('#results').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">'
-                    +'<strong>Errore!</strong> Si è verificato un errore durante l\'invio della richiesta.'
-                    +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
-                        +'<span aria-hidden="true">&times;</span>'
-                    +'</button>'
-                +'</div>');
-            }
+                        var result = "";
 
-        });
+                        $(data).each(function(index, item) {
+                            result += "<i>Richiesta " + (index+1) + "</i> <br> ";
+                            result += "user_id: " + item.user_id + " <br> ";
+                            result += "title: " + item.title + " <br> ";
+                            result += "language: " + item.language + " <br> ";
+                            result += "framework: " + item.framework + " <br> ";
+                            result += "dataset: " + item.dataset + " <br> ";
+                            result += "dataset_datatype: " + item.dataset_datatype + " <br> ";
+                            result += "model: " + item.model + " <br> ";
+                            result += "status: " + item.status + " <br> ";
+                            result += "created_at: " + item.created_at + " <br> ";
+                            result += "job_id: " + item.job_id + " <br> ";
+                            result += "<br>";
+        
+                        });
+        
+                        $('#results').html('<div class="alert alert-info" role="alert">'
+                            +'<strong>Dati richiesti</strong> <br><br>'+result
+                        +'</div>');
+
+                    }
+
+                    
+                },
+                error: function() {
+                    $('#results').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">'
+                        +'<strong>Errore!</strong> Si è verificato un errore durante l\'invio della richiesta.'
+                        +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+                            +'<span aria-hidden="true">&times;</span>'
+                        +'</button>'
+                    +'</div>');
+                }
+    
+            });
+
+        } 
 
     });
 
@@ -53,40 +82,67 @@ $(document).ready(function() {
     $("#get-single-request").click(function(e){
         e.preventDefault(); 
 
-        $.ajax({
+        var job_id = $("#job_id").val();
 
-            type: "GET",
-            url: API + 'jobs/' + $("#job_id").val(),
-            dataType: "json",
-            success: function(data) {
+        if( !job_id ) {
+            $('#results').html('<div class="alert alert-warning alert-dismissible fade show" role="alert">'
+                +'<strong>Warning!</strong> Compila il campo job_id.'
+                +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+                    +'<span aria-hidden="true">&times;</span>'
+                +'</button>'
+            +'</div>');
+        }
+        else {
 
-                var result = "";
-                result += "user_id: " + data.user_id + " <br> ";
-                result += "title: " + data.title + " <br> ";
-                result += "language: " + data.language + " <br> ";
-                result += "framework: " + data.framework + " <br> ";
-                result += "dataset: " + data.dataset + " <br> ";
-                result += "dataset_datatype: " + data.dataset_datatype + " <br> ";
-                result += "model: " + data.model + " <br> ";
-                result += "status: " + data.status + " <br> ";
-                result += "created_at: " + data.created_at + " <br> ";
-                result += "job_id: " + data.job_id + " <br> ";
-                result += "<br>";
+            $.ajax({
 
-                $('#results').html('<div class="alert alert-info" role="alert">'
-                    +'<strong>Dati richiesti</strong> <br><br>'+result
-                +'</div>');
-            },
-            error: function() {
-                $('#results').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">'
-                    +'<strong>Errore!</strong> Si è verificato un errore durante l\'invio della richiesta.'
-                    +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
-                        +'<span aria-hidden="true">&times;</span>'
-                    +'</button>'
-                +'</div>');
-            }
+                type: "GET",
+                url: API + 'jobs/' + job_id,
+                dataType: "json",
+                success: function(data) {
+                    
+                    if(!data){
+                        $('#results').html('<div class="alert alert-warning alert-dismissible fade show" role="alert">'
+                                +'<strong>Warning!</strong> Questo job_id non esiste.'
+                                +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+                                    +'<span aria-hidden="true">&times;</span>'
+                                +'</button>'
+                            +'</div>');
+                    }
+                    else{
 
-        });
+                        var result = "";
+                        result += "user_id: " + data.user_id + " <br> ";
+                        result += "title: " + data.title + " <br> ";
+                        result += "language: " + data.language + " <br> ";
+                        result += "framework: " + data.framework + " <br> ";
+                        result += "dataset: " + data.dataset + " <br> ";
+                        result += "dataset_datatype: " + data.dataset_datatype + " <br> ";
+                        result += "model: " + data.model + " <br> ";
+                        result += "status: " + data.status + " <br> ";
+                        result += "created_at: " + data.created_at + " <br> ";
+                        result += "job_id: " + data.job_id + " <br> ";
+                        result += "<br>";
+        
+                        $('#results').html('<div class="alert alert-info" role="alert">'
+                            +'<strong>Dati richiesti</strong> <br><br>'+result
+                        +'</div>');
+
+                    }
+ 
+                },
+                error: function() {
+                    $('#results').html('<div class="alert alert-danger alert-dismissible fade show" role="alert">'
+                        +'<strong>Errore!</strong> Si è verificato un errore durante l\'invio della richiesta.'
+                        +'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+                            +'<span aria-hidden="true">&times;</span>'
+                        +'</button>'
+                    +'</div>');
+                }
+    
+            });
+
+        }
 
     });
 
